2c55162f26deeaf0b64248244c4039e8c9941151
Esther Brunner <esther.brunner@zeix.com>
2024-05-01 11:35:25 +0200

    add a11y and interactions addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,8 @@ const config = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
     "@chromatic-com/storybook",
   ],
   framework: {
